48d4c8c3849ad3c203218790daea88be12d738a1
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-03 15:52:04 +0300

    test pre commit

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import "./App.scss";
+import React from 'react';
+import './App.scss';
 function App() {
-  console.log("test lint");
+  console.log('test lint');
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello</p>
+        <p>Hello1</p>
       </header>
     </div>
   );
