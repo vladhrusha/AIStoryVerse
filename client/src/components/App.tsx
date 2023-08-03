d09281085b442d4c1cd33b18985ea2a1a5cd8183
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-03 16:01:57 +0300

    test lint 3

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.scss';
 function App() {
-  console.log('test lint2');
+  console.log('test lint3');
   return (
     <div className="App">
       <header className="App-header">
