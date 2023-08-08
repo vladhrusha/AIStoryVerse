5ec2d48e643b41dbb40892e816d64dc8abbf58b6
Vlad Hrusha <hrushavladyslavwork@gmail.com>
2023-08-08 18:19:11 +0300

    added time constratins of 5 sec to voting

--- server/src/modules/vote/vote.database.ts
+++ server/src/modules/vote/vote.database.ts
@@ -1,33 +1,32 @@
 import mongoose from 'mongoose';
 
 import Vote from '../../models/Vote';
-import User from '../../models/User';
 import Story from '../../models/Story';
-import * as userDb from '../user/user.database';
 
 import logger from '../../../tools/logger';
+
 export const addVote = async ({ value, userId, storyId }) => {
-  const story = await Story.findOne({ id: storyId });
   const vote = await Vote.findOne({ storyId: storyId, voterId: userId });
 
-  const now = new Date();
-  const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
+  const fiveSecondsAgo = new Date(Date.now() - 5 * 1000);
 
-  //   const recentVote = await Vote.findOne({
-  //     storyId: storyId,
-  //     voterId: userId,
-  //     timestamp: { $lt: oneHourAgo },
-  //   });
-
-  if (!(vote && vote.timestamp)) {
-    const newVote = new Vote({
-      storyId: storyId,
-      voterId: userId,
-      value,
-    });
-    await newVote.save();
-    return 'voted';
-  }
+  if (vote.timestamp > fiveSecondsAgo)
+    return 'You can only vote once per 5 seconds';
+  if (vote.timestamp)
+    if (!vote) {
+      const newVote = new Vote({
+        storyId: storyId,
+        voterId: userId,
+        value,
+      });
+      await newVote.save();
+      return 'voted created';
+    } else {
+      vote.value = value;
+      vote.timestamp = new Date(Date.now());
+      await vote.save();
+      return 'vote updated';
+    }
 };
 
 export const calculateRatings = async () => {
